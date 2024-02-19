f8b96450ee888c7f93ce09a07de3cf3012118761
Karam Nassar <karam.nassar@hotmail.com>
2024-02-19 13:14:41 +0300

    Madlibs game

---
+++ Madlibs/madlibs.py
@@ -0,0 +1,16 @@
+# youtuber = "Karam"
+# print("Subscribe to {}".format(youtuber))
+# print(f"Subscribe to {youtuber}")
+# print("Subscribe to " + youtuber)
+
+adj = input('Adjective: ')
+verb1 = input('Verb in past: ')
+verb2 = input('Verb in past: ')
+famous_person = input('Famous Person')
+
+madlib = (f'In a quiet village, a young girl named {famous_person} discovered a hidden treasure in the old oak tree. '
+          f'{adj}, she {verb1} her find with her best friend, Alex. Together, they {verb2} on a thrilling adventure to '
+          f'uncover the secrets of the treasure. In the end, they learned that true friendship was the greatest '
+          f'treasure of all.')
+
+print(madlib)
